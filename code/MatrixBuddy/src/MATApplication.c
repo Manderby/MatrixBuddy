309ea0cde6a4938b83d2e7bb24d5e7b5917a9e85
Tobias Stamm <manderby@manderby.com>
2024-09-01 10:14:42 +0200

    last cmake improvements

--- code/MatrixBuddy/src/MATApplication.c
+++ code/MatrixBuddy/src/MATApplication.c
@@ -104,8 +104,8 @@ void preStartup(void* arg){
   mat_App = naAlloc(MATApplication);
   
 //  naSetApplicationName("Matrix Buddy");
-  naSetApplicationVersionString(MAT_OFFICIAL_VERSION_STRING);
-  naSetApplicationBuildString(MAT_BUILD_NUMBER_VERSION_STRING);
+  naSetApplicationVersionString(MAT_VERSION_STRING);
+  naSetApplicationBuildString(MAT_BUILD_NUMBER_STRING);
 //  naSetApplicationIconPath("icon.png");
 
   initTranslations();
